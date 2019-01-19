734f1da70ae07d18b5d1a82a9d12027322bf005a
Anh Tuan <tuanna.hsp@gmail.com>
2019-01-19 10:08:57 +0900

    Initial Commit

---
+++ kanji-handwriting-swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kanji-handwriting-swift
+//
+//  Created by Nguyễn Anh Tuấn on 2019/01/19.
+//  Copyright © 2019 Nguyễn Anh Tuấn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
